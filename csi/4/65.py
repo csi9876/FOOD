4b62fa5bdc5736cd995b3efbbd8108e2c42b878e
csi <qjxltm1@gmail.com>
2023-04-18 18:01:32 +0900

    0418

--- csi/4/65.py
+++ csi/4/65.py
@@ -22,43 +22,67 @@ def split_sequence(sequence, step):
 # sin 함수 학습 데이터
 x = [i for i in np.arange(start=-10, stop=10, step=0.1)]
 train_y = [np.sin(i) for i in x]
+# 학습 데이터셋 저장
+
 
 # 하이퍼파라미터
 n_timesteps = 15
 n_features = 1
+# 입력 시퀀스 길이를 15로 정의, 입력 벡터의 크기 1로 정의
+
 
 # 시퀀스 나누기
 # train_x.shape => (samples, timesteps)
 # train_y.shape => (samples)
 train_x, train_y = split_sequence(train_y, step=n_timesteps)
 print("shape x:{} / y:{}".format(train_x.shape, train_y.shape))
+# 학습데이터 셋을 입력 시퀀스 길이만큼 나눠 입력 시퀀스 생성
+
+
 
 # RNN 입력 벡터 크기를 맞추기 위해 벡터 차원 크기 변경
 # reshape from [samples, timesteps] into [samples, timesteps, features]
 train_x = train_x.reshape(train_x.shape[0], train_x.shape[1], n_features)
 print("train_x.shape = {}".format(train_x.shape))
 print("train_y.shape = {}".format(train_y.shape))
+# 2차원 x모델을 3차원 형태로 변환
+
+
 
 # RNN 모델 정의
 model = Sequential()
 model.add(SimpleRNN(units=10, return_sequences=False, input_shape=(n_timesteps, n_features)))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
+# RNN계층정의 후 모델 생성
+# simple RNN 계층
+# 입력 데이터 형상 정의
+# 손실함수 mse
+# 옵티마이저 adam
+
+
 
 # 모델 학습
 np.random.seed(0)
 from tensorflow.keras.callbacks import EarlyStopping
 early_stopping = EarlyStopping(monitor='loss', patience=5, mode='auto')
 history = model.fit(train_x, train_y, epochs=1000, callbacks=[early_stopping])
+# 모델 학습
+# 조기종료 함수 설정(급격히 손실이 증가하면)
+
 
 # loss 그래프 생성
 plt.plot(history.history['loss'], label="loss")
 plt.legend(loc="upper right")
 plt.show()
 
+
 # 테스트 데이터셋 생성
 test_x = np.arange(10, 20, 0.1)
 calc_y = np.cos(test_x) # 테스트 정답 데이터
+# RNN모델을 테스트 하기위해
+# cos()를 이용하여 원본 데이터에 주기적 차이를 준다
+
 
 # RNN 모델 예측 및 로그 저장
 test_y = calc_y[:n_timesteps]
@@ -68,6 +92,9 @@ for i in range(len(test_x) - n_timesteps):
     train_y = model.predict(net_input, verbose=0)
     print(test_y.shape, train_y.shape, i, i + n_timesteps)
     test_y = np.append(test_y, train_y)
+# 예측값을 저장
+
+
 
 # 예측 결과 그래프 그리기
 plt.plot(test_x, calc_y, label="ground truth", color="orange")
@@ -75,8 +102,8 @@ plt.plot(test_x, test_y, label="predicitons", color="blue")
 plt.legend(loc='upper left')
 plt.ylim(-2, 2)
 plt.show()
-
-
+# 출력
+# 오차가 거의 없다
 
 '''
 shape x:(185, 15) / y:(185,)
