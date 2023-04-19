6985e7a3e1752bfdfc0c71ce8436210cf80e85f6
csi <qjxltm@naver.com>
2023-04-20 00:18:28 +0900

    0419

--- csi/4/68.py
+++ csi/4/68.py
@@ -51,11 +51,15 @@ sent_tokenizer.fit_on_texts(sentences)
 tag_tokenizer = preprocessing.text.Tokenizer(lower=False) # 태그 정보는 lower= False 소문자로 변환하지 않는다.
 tag_tokenizer.fit_on_texts(tags)
 
+
+
 # 단어 사전 및 태그 사전 크기
 vocab_size = len(sent_tokenizer.word_index) + 1
 tag_size = len(tag_tokenizer.word_index) + 1
 print("BIO 태그 사전 크기 :", tag_size)
 print("단어 사전 크기 :", vocab_size)
+
+
 
 # 학습용 단어 시퀀스 생성
 x_train = sent_tokenizer.texts_to_sequences(sentences)
@@ -63,18 +67,26 @@ y_train = tag_tokenizer.texts_to_sequences(tags)
 print(x_train[0])
 print(y_train[0])
 
+
+
 # index to word / index to NER 정의
 index_to_word = sent_tokenizer.index_word # 시퀀스 인덱스를 단어로 변환하기 위해 사용
 index_to_ner = tag_tokenizer.index_word # 시퀀스 인덱스를 NER로 변환하기 위해 사용
 index_to_ner[0] = 'PAD'
+
+
 
 # 시퀀스 패딩 처리
 max_len = 40
 x_train = preprocessing.sequence.pad_sequences(x_train, padding='post', maxlen=max_len)
 y_train = preprocessing.sequence.pad_sequences(y_train, padding='post', maxlen=max_len)
 
+
+
 # 학습 데이터와 테스트 데이터를 8:2 비율로 분리
 x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=.2, random_state=0)
+
+
 
 # 출력 데이터를 원-핫 인코딩
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=tag_size)
@@ -84,6 +96,8 @@ print("학습 샘플 시퀀스 형상 : ", x_train.shape)
 print("학습 샘플 레이블 형상 : ", y_train.shape)
 print("테스트 샘플 시퀀스 형상 : ", x_test.shape)
 print("테스트 샘플 레이블 형상 : ", y_test.shape)
+
+
 
 # 모델 정의(Bi-LSTM)
 from tensorflow.keras.models import Sequential
@@ -96,6 +110,7 @@ model.add(TimeDistributed(Dense(tag_size, activation='softmax')))
 model.compile(loss='categorical_crossentropy', optimizer=Adam(0.01), metrics=['accuracy'])
 model.fit(x_train, y_train, batch_size=128, epochs=10)
 print("평가 결과 : ", model.evaluate(x_test, y_test)[1])
+# 순차모델
 
 
 # 시퀀스를 NER 태그로 변환
